83474033268bf3fbe097e6a09ea8696573688a4a
Philip Vuong <philipvuong@yahoo.com>
2018-10-10 19:58:51 +0000

    Adding a Strikethrough

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -6,7 +6,8 @@
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' + " data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</label></div></li>';
+      var liClass = task.done? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id + '"class="' + liClass + '"><div class="view"><input class="toggle" type="checkbox"' + " data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</label></div></li>';
       return liElement;
     }
 
